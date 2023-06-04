a6ee10a4f98614891fedf67fc164561e36759808
Andrew <andrewmfabbro@gmail.com>
2023-06-03 22:18:00 -0400

    added rendered cars to car list

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,5 +1,27 @@
-function CarList(){
-    return<div>CarList</div>
+import { useSelector } from "react-redux";
+
+function CarList() {
+  const cars = useSelector((state) => {
+    return state.cars.data; //big state obj accesses the other obj
+  });
+
+  const handleCarDelete = (car) => {
+    //...
+  };
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button className="button is-danger"
+        onClick={()=> handleCarDelete(car)}></button>
+      </div>
+    );
+  });
+
+  return <div className="car-list">{renderedCars}<hr/></div>;
 }
 
 export default CarList;
