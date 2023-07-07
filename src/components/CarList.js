4e7c00035cbb431b1125d40b3c840ab479deac21
Andrew <andrewmfabbro@gmail.com>
2023-07-07 16:23:13 -0400

    added search filter and value totaling

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,15 +1,17 @@
-import { useSelector, useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { removeCar } from "../store/slices/carsSlice";
 
 function CarList() {
   const dispatch = useDispatch();
-  const cars = useSelector((state) => {
-    return state.cars.data; //big state obj accesses the other obj
+  //destructure for the selector
+  const cars = useSelector(({ cars: { data, searchTerm } }) => {
+    return data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
   });
 
   const handleCarDelete = (car) => {
     dispatch(removeCar(car.id));
-
   };
 
   const renderedCars = cars.map((car) => {
@@ -18,13 +20,22 @@ function CarList() {
         <p>
           {car.name} - ${car.cost}
         </p>
-        <button className="button is-danger"
-        onClick={()=> handleCarDelete(car)}>Delete Car</button>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete Car
+        </button>
       </div>
     );
   });
 
-  return <div className="car-list">{renderedCars}<hr/></div>;
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
